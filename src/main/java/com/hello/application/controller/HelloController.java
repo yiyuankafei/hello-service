8a9c2cef89e10fe6cfb208a70b06ec97c84d4148
byf <124633367@qq.com>
2020-09-02 02:39:26 +0800

    test

---
+++ src/main/java/com/hello/application/controller/HelloController.java
@@ -0,0 +1,17 @@
+package com.hello.application.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/hello")
+public class HelloController {
+	
+	@RequestMapping("/test")
+	public String test() {
+		
+		return "here is hello-service";
+		
+	}
+
+}
